58c68bce92ab1265a87bf40919e81914f6b286dc
Caalb <carlosmunizfjunior@gmail.com>
2023-07-15 20:05:28 -0300

    feat: add express setup and hello word route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import * as express from "express";
+import { Request, Response } from "express";
+
+const app = express();
 class Person {
-  sayMaName() {
-    return "Carlos";
+  sayHello() {
+    return "Hello World";
   }
 }
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(new Person().sayHello());
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
 
 export default Person;
